a09a88047f1c8c069d66eac518bffe9ea9233564
aditya18saxena-afk <aditya18.saxena@gmail.com>
2025-09-27 11:46:40 +0000

    datatypes

---
+++ basics_001/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "Aditya@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 //not allowed
+accountEmail = "ha@ndj.com"
+accountPassword = "7362876"
+accountCity = "jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+this is a multiple line comment , prefer not to use var becoz of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ basics_001/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js codes as newer version
+
+// alert(3+3)  // we are using nodejs not browser
+
+console.log(3
+    +
+    3)  // code readibility ends here dude , it should be high
+
+console.log("aditya")
+
+
+
+console.log(typeof undefined);
+

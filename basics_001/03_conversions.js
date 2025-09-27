57f0bbe74d11bb7b6674e74b1187ff3fa3fbeb59
aditya18saxena-afk <aditya18.saxena@gmail.com>
2025-09-27 12:26:56 +0000

    conversion

---
+++ basics_001/03_conversions.js
@@ -0,0 +1,23 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Aditya"
+let booleanisloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisloggedIn);
+
+// 1=true ; 0=false
+//""=false
+//"Aditya" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

dbc185f6c196c763d02629b76220c467b7406cf2
aditya18saxena-afk <aditya18.saxena@gmail.com>
2025-10-03 10:02:12 +0000

    objects

--- basics_002/11_objects.js
+++ basics_002/11_objects.js
@@ -28,10 +28,44 @@ console.log(regularUser.fullname.userFullName.fisrtname);
 
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"c",4:"d"}
+const obj4 = {5:"c",6:"d"}
 
-const obj3 ={obj1,obj2}
+
+// const obj3 ={obj1,obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2,...obj4}
 console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // object ko array mei badal diya.
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); //array ke andar array.
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
 
 
 // console.log(tinderUser);
+
+
 
